3c98bf153b4cbbf2f8c24038186ad2239685e9bd
miko550 <83682793+miko550@users.noreply.github.com>
2026-04-08 10:15:20 +0800

    Update SSH service commands for newer versions

--- setup_kali_ssh.sh
+++ setup_kali_ssh.sh
@@ -4,9 +4,7 @@ sudo apt update
 sudo apt install openssh-server
 
 echo "[i] Start the SSH service"
-sudo service ssh start
 sudo systemctl start ssh (for newer versions)
 
 echo "[i] Enable SSH service to start on boot"
-sudo service ssh enable
 sudo systemctl enable ssh
